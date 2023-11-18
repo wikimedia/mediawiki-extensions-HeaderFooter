fcd26bb4113996ff9a4e3e49fadcb14c56fdeb03
zoranzoki21 <zorandori4444@gmail.com>
2023-11-18 19:25:52 +0100

    Enable Grunt + make tests pass
    Change-Id: If4bd09ebaee9759ca8c18aeb2eaeaf6a041c5a2b

--- modules/dynamicload.js
+++ modules/dynamicload.js
@@ -1,18 +1,17 @@
-
 var egHeaderFooter = mw.config.get( 'egHeaderFooter' );
 
 if ( egHeaderFooter.enableAsyncHeader ) {
-	var extHeaderFooterBlocks = [ "hf-nsheader", "hf-header" ];
-}
-else {
+	var extHeaderFooterBlocks = [ 'hf-nsheader', 'hf-header' ];
+} else {
+	// eslint-disable-next-line no-redeclare
 	var extHeaderFooterBlocks = [];
 }
-extHeaderFooterBlocks = extHeaderFooterBlocks.concat( [ "hf-footer", "hf-nsfooter" ] );
+extHeaderFooterBlocks = extHeaderFooterBlocks.concat( [ 'hf-footer', 'hf-nsfooter' ] );
 
 for ( var i = 0; i < extHeaderFooterBlocks.length; i++ ) {
-	var block = extHeaderFooterBlocks[i];
+	var block = extHeaderFooterBlocks[ i ];
 
-	$( "." + block ).each( function( i, e ) {
+	$( '.' + block ).each( function ( i, e ) {
 
 		// FIXME: At some point, put some method of indicating unloaded content here
 
@@ -23,25 +22,25 @@ for ( var i = 0; i < extHeaderFooterBlocks.length; i++ ) {
 
 		// Message ID of block (header or footer) is in the HTML ID. hf-nsheader
 		// will have an ID like hf-nsheader-Help for the help namespace.
-		var msgId = $(e).attr('id');
+		var msgId = $( e ).attr( 'id' );
 
 		$.get(
-			mw.config.get("wgScriptPath") + "/api.php",
+			mw.config.get( 'wgScriptPath' ) + '/api.php',
 			{
-				action: "getheaderfooter",
+				action: 'getheaderfooter',
 				messageid: msgId,
-				contexttitle: mw.config.get('wgPageName'),
-				format: "json"
+				contexttitle: mw.config.get( 'wgPageName' ),
+				format: 'json'
 			},
 			function ( response ) {
 				// var blockText = response.query.allmessages[0]["*"];
 				var blockText = response.getheaderfooter.result;
-				$( "#" + msgId ).html( blockText );
-				$( "#" + msgId ).find( "#headertabs" ).each( function(i,e) {
-					$(e).tabs();
-				});
+				$( '#' + msgId ).html( blockText );
+				$( '#' + msgId ).find( '#headertabs' ).each( function ( i, e ) {
+					$( e ).tabs();
+				} );
 			}
-		)
+		);
 
 	} );
 
