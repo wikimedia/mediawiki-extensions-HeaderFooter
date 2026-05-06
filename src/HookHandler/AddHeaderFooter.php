8536c86147ca0946b17dfa7f8abe5c15ace0c3b2
Hamish <hamish.slater@hallowelt.com>
2026-05-06 18:03:45 +0700

    Replace deprecated ParserOutput::getRawText
    ERM20742
    
    [master]
    
    Change-Id: I971fd84a4306ff2b553fd0b82725fa0dc722a5a2

--- src/HookHandler/AddHeaderFooter.php
+++ src/HookHandler/AddHeaderFooter.php
@@ -43,7 +43,7 @@ class AddHeaderFooter implements ContentAlterParserOutputHook {
 		$footer   = $this->generateHeaderFooter( 'hf_footer', 'hf-footer', $name, $parserOutput );
 		$nsfooter = $this->generateHeaderFooter( 'hf_nsfooter', 'hf-nsfooter', $ns, $parserOutput );
 
-		$text = $parserOutput->getRawText();
+		$text = $parserOutput->getContentHolderText();
 		$parserOutput->setContentHolderText( $nsheader . $header . $text . $footer . $nsfooter );
 		$parserOutput->setExtensionData( 'HeaderFooter-added', true );
 
